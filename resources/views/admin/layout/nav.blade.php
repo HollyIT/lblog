4598ed138357c45ae14d89ba4ce1bb920844367a
Jamie Holly <jamie@hollyit.net>
2021-10-24 08:17:36 -0400

    Removed Route facade from admin.layout.nav and replace with $currentRouteName supplied via view composer.

--- resources/views/admin/layout/nav.blade.php
+++ resources/views/admin/layout/nav.blade.php
@@ -1,7 +1,3 @@
-<?php
-$currentRoute = \Illuminate\Support\Facades\Route::currentRouteName();
-?>
-
 <aside
     style="position: fixed;top:0;left:0;bottom:0;width:220px;"
     class="z-20 hidden w-64 overflow-y-auto bg-gray-800 dark:bg-gray-800 md:block flex-shrink-0"
@@ -16,27 +12,27 @@ $currentRoute = \Illuminate\Support\Facades\Route::currentRouteName();
             @component('admin.layout.nav_item', [
             'label' => 'Posts',
             'to' => route('admin.posts.index'),
-            'active' => str_starts_with($currentRoute, 'admin.posts.')
+            'active' => str_starts_with($currentRouteName, 'admin.posts.')
             ])
                 @include('admin.icons.post')
             @endcomponent
             @component('admin.layout.nav_item', [
                 'label' => 'Images',
                 'to' => route('admin.images.index'),
-                'active' => str_starts_with($currentRoute, 'admin.images.')
+                'active' => str_starts_with($currentRouteName, 'admin.images.')
                 ])
                 @include('admin.icons.images')
             @endcomponent
 
             @can('viewAny', \App\Models\User::class)
-            @component('admin.layout.nav_item', [
-                'label' => 'Users',
-                'to' => route('admin.users.index'),
-                'active' => str_starts_with($currentRoute, 'admin.users.')
-                ])
-                @include('admin.icons.users')
-            @endcomponent
-                @endcan
+                @component('admin.layout.nav_item', [
+                    'label' => 'Users',
+                    'to' => route('admin.users.index'),
+                    'active' => str_starts_with($currentRouteName, 'admin.users.')
+                    ])
+                    @include('admin.icons.users')
+                @endcomponent
+            @endcan
 
 
         </ul>
